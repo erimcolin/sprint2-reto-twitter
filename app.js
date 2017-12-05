70099c620b687519a8fd7e23aad7291832868470
erimcolin <gabymcolin@gmail.com>
2017-12-05 03:00:09 -0600

    cambiando nombres variables

--- app.js
+++ app.js
@@ -1,63 +1,55 @@
 //se mandan traer los elementos del HTML al JS
 var boton = document.getElementById("sent-text");
-var texto = document.getElementById("text");
-var nuevoParrafo = document.getElementById("contenedor");
-var contadorLetras = document.getElementById("contadorDeCaracteres");
-
+var text = document.getElementById("text");
+var newParraf = document.getElementById("container");
+var letterCount = document.getElementById("characteres");
 //version 0.0.1
 //se agrega el evento click en el boton de tweet para que imprima los mensajes
 boton.addEventListener("click", twittear)
 
 function twittear(event) {
-   if (texto.value) {
-     var parrafo = document.createElement("p");
-     var divContenedor = document.createElement("div");
-     parrafo.textContent = texto.value;
-     divContenedor.appendChild(parrafo);
-     nuevoParrafo.appendChild(divContenedor);
-     divContenedor.classList.add("content");
-     texto.value = "";
-     parrafo.style.marginLeft = '14px';
+   if (text.value) {
+     var parraf = document.createElement("p");
+     var divContainer = document.createElement("div");
+     parraf.textContent = text.value;
+     divContainer.appendChild(parraf);
+     newParraf.appendChild(divContainer);
+     divContainer.classList.add("content");
+     // texto.value = "";
+     parraf.style.marginLeft = '14px';
     // parrafo.style.border = '10px';
-
-  /*   (function () {
-       var fecha = new Date();
-       document.write(fecha);
-     } ())
-*/
-
-     /*falta darle estilos al nuevo parrafo para que se acomoden
-     debajo del contenedor, con un margen a la izquiera */
    }
 };
 // version 0.0.2
 //se agrega un contador que lleva la cuenta regresiva
-texto.addEventListener("keyup", contadorCaracteres);
+text.addEventListener("keyup", countCharacters);
 
-function contadorCaracteres(event){
-  var contador = 0;
-  var letra = texto.value.length;
-  contador = texto.value.length;
-  contadorLetras.value = 140 - contador;
+function countCharacters(event){
+  var count = 0;
+  // var letter = text.value.length;
+  count = text.value.length;
+  letterCount.value = 140 - count;
 /*
 version 0.0.3
-1.-Si pasa los 140 caracteres se desabilita el boton
+1.-Si pasa los 140 caracteres se desabilita el boton y cambia de color
 2. si para los 120 caracteres, mostrar el boton en otro color
 ... etc */
-    if ( contador > 140) {
-    boton.disabled = true;
-    contadorLetras.style.color = "black";
-    //boton.style.color = 'grey';
-    } else if (contador >  119 && contador < 130) {
-        contadorLetras.style.color = "orange";
-    } else if (contador > 129 && contador < 140) {
-        contadorLetras.style.color = "red";
+    if ( count > 140) {
+        boton.disabled = true;
+        letterCount.style.color = "black";
+        boton.style.color = 'grey';
+        boton.style.background = 'lightgrey';
+    } else if (count >  119 && count < 130) {
+        letterCount.style.color = "orange";
+    } else if (count > 129 && count < 140) {
+        letterCount.style.color = "red";
     } else {
         boton.disabled = false;
-        contadorLetras.style.color = "blue";
+        letterCount.style.color = "blue";
+        boton.style.color = 'white';
+        boton.style.background = '#6ad2f3'
     }
 };
-
 /*version 0.0.4
 al presionar enter el textarea crece de acuerdo al tamaño del texto */
 var textarea = document.querySelector('textarea');
@@ -70,15 +62,16 @@ function autosize(){
     el.style.cssText = 'height:' + el.scrollHeight + 'px';
   }, 0);
 };
-
 // agregar la hora
 // var tiempo = moment("2016-01-01").format("MMM Do YY");
 // console.log(tiempo);
-
-function mostrarHora(){
+boton.addEventListener("click", showHour)
+function showHour(event){
   momentoActual = new Date();
   hora = momentoActual.getHours();
+  minuto = momentoActual.getMinutes();
+  segundo = momentoActual.getSeconds();
 
-  horaImprimible = hora + ' : ';
-  document.getElementById('mostrarHora').innerHTML = horaImprimible;
+  horaImprimible = hora + ' : ' + minuto + ' : ' + segundo;
+  document.getElementById('showHour').innerHTML = horaImprimible;
 };
